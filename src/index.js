ec9e8b77a4051492d58bfbbdacf1edb0f252dd05
orestSharak <sharak.orest@gmail.com>
2018-04-21 17:44:18 +0200

    new cleans in App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom';
-import Bootstrap from "bootstrap-without-jquery";
+import Bootstrap from "bootstrap-without-jquery"; // for feature bootstrap
 import style from "./css/main.css";
-import Header from "./components/Header";
+import Home from "./components/Home";
+import Schedule from "./components/Schedule";
+import Topics from "./components/Topics";
 
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/schedule'>Schedule</Link></li>
+                        <li><Link to='/topics'>Topics</Link></li>
+                    </ul>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/schedule' component={Schedule}/>
+                    <Route path='/topics' component={Topics}/>
+                </div>
+            </Router>
+        )
+    }
+}
 
 const element = document.getElementById('root');
 ReactDOM.render(
-    <Header/>,
+    <App/>,
     element);
